5ea0d860eb1671530ebcaf128ac29431899d8e66
Henry Kirk <henry@the-kirks.com>
2016-11-30 00:12:08 -0500

    Updated style of comments page

--- assets/includes/comments.inc.php
+++ assets/includes/comments.inc.php
@@ -2,7 +2,30 @@
 <!-- Adapted from: https://codepen.io/magnus16/pen/buGiB?editors=1100 -->
 <div class="detailBox">
     <div class="titleBox">
-      <label>Comment Box</label>
+      <label>Leave a Comment</label>
+
+      <form class="form-inline" role="form">
+            <div class="form-group comment-input">
+                <input class="form-control"
+                       name="name"
+                       id="name"
+                       type="text" 
+                       placeholder="Your name"
+                       maxlength="30" />
+            </div>
+            <div class="form-group comment-input">
+                <input class="form-control"
+                       name="comment"
+                       id="comment"
+                       type="text" 
+                       placeholder="Your comment" 
+                       maxlength="30" />
+            </div>
+            <div class="form-group comment-btn">
+                <button class="btn btn-default"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span></button>
+            </div>
+        </form>
+
     </div>
     <div class="actionBox">
         <ul class="commentList">
@@ -25,26 +48,5 @@
                 </div>
             </li>
         </ul>
-        <form class="form-inline" role="form">
-            <div class="form-group" style="position: relative; top: 15px;">
-                <input class="form-control"
-                       name="name"
-                       id="name"
-                       type="text" 
-                       placeholder="Your name"
-                       maxlength="30" />
-            </div>
-            <div class="form-group">
-                <input class="form-control"
-                       name="comment"
-                       id="comment"
-                       type="text" 
-                       placeholder="Your comment" 
-                       maxlength="30" />
-            </div>
-            <div class="form-group" style="width:19%;">
-                <button class="btn btn-default">Add</button>
-            </div>
-        </form>
     </div>
 </div>
